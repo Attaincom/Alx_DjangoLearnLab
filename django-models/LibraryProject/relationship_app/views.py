256ede4753e15f5900f5266fee434b3b7b6bff96
Attaincom <olatomidefagbohun@gmail.com>
2025-09-03 14:25:27 +0100

    new relationship apps

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library  # must include Library
 
-
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
-
 
 # Class-based view to show library details
 class LibraryDetailView(DetailView):
