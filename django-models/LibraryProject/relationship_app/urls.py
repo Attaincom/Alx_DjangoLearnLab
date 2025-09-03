6396cc6bf55737462a5d2ce0b8f337654ecc5e34
Attaincom <olatomidefagbohun@gmail.com>
2025-09-03 16:22:52 +0100

    New Initial commit: relationship_app and django-models project init

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import list_books
-from .views import LibraryDetailView
-from .views import register as register  # ensures checker sees views.register
-from .views import admin_view, librarian_view, member_view
+from .views import (
+    list_books,
+    LibraryDetailView,
+    register,
+    admin_view,
+    librarian_view,
+    member_view,
+    add_book,
+    edit_book,
+    delete_book
+)
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -16,4 +23,9 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+
+    # Book CRUD URLs
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
 ]
