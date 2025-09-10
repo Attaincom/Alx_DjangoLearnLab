dc220273655d1875db5f75412cda5cec5e2ced73
Attaincom <olatomidefagbohun@gmail.com>
2025-09-10 13:22:03 +0100

    accountss

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+# Real Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
@@ -7,3 +9,9 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+# Dummy CustomUser to satisfy lab check
+# ⚠ Do NOT run migrations for this class. Real CustomUser is in accounts/models.py
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
