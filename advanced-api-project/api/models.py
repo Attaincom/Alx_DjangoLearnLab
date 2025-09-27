6ec86a7c7284ca2b3b6b041868357addfd2b1720
Attaincom <olatomidefagbohun@gmail.com>
2025-09-27 11:55:25 +0100

    advanced api

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Author(models.Model):
+    """
+    Author model stores details of book authors.
+    One Author can have many Books (one-to-many relationship).
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Book model stores details of books.
+    Each book belongs to one Author (via ForeignKey).
+    """
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
